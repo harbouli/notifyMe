24b0f7c9f421ab623256452c3ead6edc1447ecfc
harbouli <mohamed.harbouli.hb@gmail.com>
2025-09-27 22:33:44 +0100

    feat: add GetByExternalID method to MockUserRepository for testing

--- internal/application/usecase/auth_usecase_test.go
+++ internal/application/usecase/auth_usecase_test.go
@@ -48,6 +48,14 @@ func (m *MockUserRepository) GetByUsername(ctx context.Context, username string)
 	return args.Get(0).(*entity.User), args.Error(1)
 }
 
+func (m *MockUserRepository) GetByExternalID(ctx context.Context, externalID string) (*entity.User, error) {
+	args := m.Called(ctx, externalID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*entity.User), args.Error(1)
+}
+
 func (m *MockUserRepository) Update(ctx context.Context, user *entity.User) error {
 	args := m.Called(ctx, user)
 	return args.Error(0)
